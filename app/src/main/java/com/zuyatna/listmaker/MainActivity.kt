4c54c0895f618415b2cffadf8e41aadeb32b3f5b
Suyatna <zuyatna@gmail.com>
2022-01-20 17:07:52 +0700

    chapter8 - adding a FAB and a dialog

--- app/src/main/java/com/zuyatna/listmaker/MainActivity.kt
+++ app/src/main/java/com/zuyatna/listmaker/MainActivity.kt
@@ -2,17 +2,54 @@ package com.zuyatna.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.zuyatna.listmaker.databinding.MainActivityBinding
 import com.zuyatna.listmaker.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+
+        binding = MainActivityBinding.inflate(layoutInflater)
+
+        val view = binding.root
+        setContentView(view)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.fabButton.setOnClickListener {
+            showCreateListDialog()
+        }
+    }
+
+    private fun showCreateListDialog() {
+        // 1
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        // 2
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+
+        // 3
+        builder.setPositiveButton(positiveButtonTitle) { dialog, _ ->
+            dialog.dismiss()
+        }
+
+        // 4
+        builder.create().show()
     }
 }
